46d3839ddd8dfa9aff41b18be44d048ffb4fe770
Li Yuhan <3188127343@qq.com>
2025-11-28 20:37:03 +0800

    Update autosign.js

--- courses.zju/autosign.js
+++ courses.zju/autosign.js
@@ -1,5 +1,4 @@
-import loginZju from "login-zju";
-const { COURSES, ZJUAM } = loginZju;
+import { COURSES, ZJUAM } from "login-zju";
 import { v4 as uuidv4 } from "uuid";
 import "dotenv/config";
 import crypto from "crypto";
